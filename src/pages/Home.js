facc1c852b37b0fe2cf5b6f8560696f096e16717
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-12-11 11:39:02 +0530

    develop home page: slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react"
 
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
+import Slider from "../components/Slider"
 
 const Home = () => {
     return(
         <Fragment>
             <Announcement />
             <Navbar />
+            <Slider />
         </Fragment>
     )
 }
